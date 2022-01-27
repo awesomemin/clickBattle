ae28360f2be9a60adea8ed7941d397f9b24b02c7
Sungmin Oh <saogig0503@naver.com>
2022-01-27 23:11:05 +0900

    apply dotenv to improve secure

--- config/config.js
+++ config/config.js
@@ -1,7 +1,9 @@
-{
+require('dotenv').config();
+
+module.exports = {
   "development": {
     "username": "root",
-    "password": "",
+    "password": process.env.DB_PASSWORD,
     "database": "dbcon",
     "host": "127.0.0.1",
     "dialect": "mysql"
